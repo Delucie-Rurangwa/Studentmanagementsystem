688864c371bfc78855742fbd5312364ac8481cc8
Delucie_coder <d.rurangwa@alustudent.com>
2026-05-19 10:04:21 +0200

    initial comit

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,9 @@
  public class Student {
-        // --- INSTANCE VARIABLES (Attributes) ---
-        // These hold the data for each individual student[cite: 28].
+        // --- Declaring  VARIABLES---
         String name;
         int age;
         int marks;
 
-        // --- STATIC VARIABLE ---
-        // Shared by all students to count total enrollment.
-        static int totalEnrolled = 0;
 
         // --- CONSTRUCTOR ---
         // Initializes the variables whenever a new student object is created.
@@ -15,7 +11,6 @@
             this.name = name;
             this.age = age;
             this.marks = marks;
-            totalEnrolled++; // Increments the counter each time a student is made
         }
 
         // --- METHOD 1: displayStudentInfo() ---
@@ -27,7 +22,6 @@
         }
 
         // --- METHOD 2: calculateGrade() ---
-        // A return-type method that returns a String letter grade based on marks[cite: 8, 31].
         public String calculateGrade() {
             if (marks >= 80) {
                 return "A";
@@ -53,13 +47,12 @@
         }
 
         // --- METHOD 4: schoolMotto() (STATIC) ---
-        // Belongs to the class itself. Prints the required statement[cite: 33].
+        // Belongs to the class itself. Prints the required statement
         public static void schoolMotto() {
             System.out.println("Education For Everyone");
         }
 
         // --- CHALLENGE METHOD: compareMarks() ---
-        // Compares this student's marks with another student object.
         public String compareMarks(Student otherStudent) {
             if (this.marks > otherStudent.marks) {
                 return this.name + " has higher marks!";
